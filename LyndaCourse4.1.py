5da02d0e7df9d66ef65a82f3239ab900186a4c57
coreyderosa <coreyderosa@gmail.com>
2016-10-01 21:11:42 -0600

    Window Frame
    Adjust the size and style of the window frame border

---
+++ LyndaCourse4.1.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk        
+    
+root = Tk()
+
+frame = ttk.Frame(root)
+frame.pack()
+frame.config(height = 100, width = 200) #size of the window 
+frame.config(relief = RIDGE) #different styles RIDGE, RAISED, SUNKEN, SOLID, GROOVE, FLAT
+ttk.Button(frame, text = 'Click Me').pack()
+frame.config(padding = (30, 15))
+ttk.LabelFrame(root, height = 100, width = 200, text = 'My Frame').pack()
+
+root.mainloop()
