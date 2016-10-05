28e7b1dafaeb2226e27b0bbcafe535415cd08bf5
coreyderosa <coreyderosa@gmail.com>
2016-10-04 22:15:28 -0600

    Started Survey GUI
    Added labels, entrys, text window and buttons.  Got the Clear buitton
    working.

--- Python Course Item 49 of 62.py
+++ Python Course Item 49 of 62.py
@@ -1,14 +1,49 @@
 from tkinter import *
 from tkinter import ttk
 root = Tk()
-button = ttk.Button(root, text = "Click Me!") #creates the button in the window
-button.pack() #creates the window
-button["text"] # will print Click Me
-button["text"] = "Press Me" #updates the text of the button from Click Me to Press Me
-button.config(text = "Push Me") #another way to change the wording on the button 
-button.config() #shows a list of properties for the button
-str(button) #assigns button a random number between 0 and 1
-str(root)
-ttk.Label(root, text = "Hello, Tkinter!").pack() #adds wording to the window under the Push Me button
+logo = PhotoImage(file = 'D:\Documents\School\Tech Academy\Python\desert_desc_bug.gif')
+def main():
 
+    paneTop = ttk.Panedwindow(root, orient = HORIZONTAL)
+    paneTop.grid(row = 0, column = 0, columnspan = 4, rowspan = 3, sticky = 'nw')
+    
+    paneBottom = ttk.Panedwindow(root, orient = HORIZONTAL).grid(row = 4, column = 0, columnspan = 4, rowspan = 3)
+
+    instructions = ttk.Label(paneTop, text = 'We\'re glad you are interested in California!  Below is a way for you to express your ideas regarding the Special: From Desert to Sea.  '
+                                                      'Leave your comments below!', wraplength = 300).grid(row = 0, column = 1, columnspan = 2, sticky = 'w')
+
+
+
+    desertLogo = ttk.Label(paneTop, image = logo).grid(row = 0, column = 0,  sticky = 'w')
+
+    userName = ttk.Label(paneBottom, text = 'Name: ').grid(row = 3, column = 0, sticky = 'w')
+
+    userEmail = ttk.Label(paneBottom, text = 'Email: ').grid(row = 3, column = 1, sticky = 'w')
+
+    userComments = ttk.Label(paneBottom, text = 'Comments: ').grid(row = 5, column = 0, sticky = 'w')
+
+    userNameEntry = ttk.Entry(paneBottom, width = 30)
+    userNameEntry.grid(row = 4, column = 0, sticky = 'w')
+
+    userEmailEntry = ttk.Entry(paneBottom, width = 30)
+    userEmailEntry.grid(row = 4, column = 1, sticky = 'w')
+
+    text = Text(root, width = 65, height = 12, wrap = 'word')
+    text.grid(row = 6, column = 0, columnspan = 3)
+
+    submitButton = ttk.Button(paneBottom, text = 'Submit').grid(row = 7, column = 0, sticky = 'e')
+                        
+
+
+    def clearFields():
+        text.delete('1.0', 'end')
+        userNameEntry.delete(0, 'end')
+        userEmailEntry.delete(0, 'end')
+        
+    clearButton = ttk.Button(paneBottom, text = 'Clear', command = clearFields)
+    clearButton.grid(row = 7, column = 1, sticky = 'w')    
+
+
+main()
+root.mainloop()
 
